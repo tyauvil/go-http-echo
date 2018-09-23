8d76cb75f17e8894c552693c471fa3e41fd7695e
ty <ty.auvil@gmail.com>
2018-09-23 08:47:26 -0600

    trimmed response and added logging

--- main.go
+++ main.go
@@ -6,59 +6,56 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 )
+
+func getEnv(envvar, def string) string {
+	val, env := os.LookupEnv(envvar)
+	if !env {
+		val = def
+	}
+	return val
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	type JSONResponse struct {
 		Method        string
-		URL           string
-		Proto         string
-		ProtoMajor    int
-		ProtoMinor    int
+		URL           *url.URL
+		Protocol      string
 		Header        http.Header
 		Body          io.ReadCloser
 		ContentLength int64
 		Host          string
 		RemoteAddr    string
-		RequestURI    string
 		TLS           *tls.ConnectionState
 	}
 
 	jsondata := JSONResponse{
 		Method:        r.Method,
-		URL:           r.URL.String(),
-		Proto:         r.Proto,
-		ProtoMajor:    r.ProtoMajor,
-		ProtoMinor:    r.ProtoMinor,
+		URL:           r.URL,
+		Protocol:      r.Proto,
 		Header:        r.Header,
 		Body:          r.Body,
 		ContentLength: r.ContentLength,
 		Host:          r.Host,
 		RemoteAddr:    r.RemoteAddr,
-		RequestURI:    r.RequestURI,
 		TLS:           r.TLS,
 	}
 
 	jsonbytes, err := json.Marshal(jsondata)
 	if err != nil {
-		// handle error
+		log.Println(err)
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(jsonbytes)
 }
 
-func getEnv(key, defaultValue string) string {
-	value := os.Getenv(key)
-	if len(value) == 0 {
-		return defaultValue
-	}
-	return value
-}
-
 func main() {
 	port := getEnv("PORT", "8080")
 	http.HandleFunc("/", handler)
+	log.Println("Starting go-http-echo")
+	log.Println("Listening on port:", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
